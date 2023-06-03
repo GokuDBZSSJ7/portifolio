a4829aa0a062df297bb713e7282dc6edbedcf573
Lucas Baggio <lucas.baggio.silva@gmail.com>
2023-06-03 14:41:14 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { MeComponent } from './me/me.component';
+import { TecnologiaComponent } from './tecnologia/tecnologia.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PerfilComponent,
+    MeComponent,
+    TecnologiaComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
